3af42c75a71566687f7efc8786d188ff186980e6
Paul.Inglsmit <paul.inglsmit@gmail.com>
2022-09-17 20:48:27 +0300

    #7 Sceleton loading

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
+import Sceleton from './components/PizzaBlock/Sceleton';
 import React from 'react';
 
 // import pizzas from './assets/pizzas.json';
 
 function App () {
-	const [ pizzas, setPizzas ] = React.useState( [] )
+	const [ pizzas, setPizzas ] = React.useState( [] );
+	const [ isLoading, setIsLoading ] = React.useState( true );
 
 	React.useEffect( () => {
 		fetch( 'https://63232eaf362b0d4e7dde21f1.mockapi.io/items' )
 			.then( ( res ) => res.json() )
-			.then( ( arr ) => setPizzas( arr ) );
+			.then( ( arr ) => {
+				setPizzas( arr );
+				setIsLoading( false );
+			} );
 	}, [] );
 
 	return (
@@ -27,8 +32,9 @@ function App () {
 					</div>
 					<h2 className="content__title">All pizzas</h2>
 					<div className="content__items">
-						{
-							pizzas.map( ( pizza, i ) => (
+						{ isLoading
+							? [ ...new Array( 6 ) ].map( ( _, i ) => <Sceleton key={ i } /> )
+							: pizzas.map( ( pizza, i ) => (
 								<PizzaBlock key={ i } { ...pizza } />
 							) )
 						}
---
+++ src/components/PizzaBlock/Sceleton.js
@@ -0,0 +1,24 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+const Sceleton = ( props ) => (
+	<ContentLoader
+		className="pizza-block"
+		speed={ 2 }
+		width={ 280 }
+		height={ 465 }
+		viewBox="0 0 280 465"
+		backgroundColor="#f3f3f3"
+		foregroundColor="#ecebeb"
+		{ ...props }
+	>
+		<circle cx="141" cy="125" r="125" />
+		<rect x="0" y="424" rx="10" ry="10" width="96" height="27" />
+		<rect x="171" y="416" rx="25" ry="25" width="108" height="45" />
+		<rect x="0" y="266" rx="10" ry="10" width="280" height="27" />
+		<rect x="0" y="310" rx="10" ry="10" width="280" height="88" />
+	</ContentLoader>
+)
+
+export default Sceleton
+
