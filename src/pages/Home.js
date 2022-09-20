02a5509d772d28e2781b71f3b23eeb8f7bcbf959
Paul.Inglsmit <paul.inglsmit@gmail.com>
2022-09-20 11:57:49 +0300

    #8 Cart page && ltl  fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,30 +7,35 @@ import Sceleton from '../components/PizzaBlock/Sceleton';
 export const Home = () => {
 	const [ pizzas, setPizzas ] = React.useState( [] );
 	const [ isLoading, setIsLoading ] = React.useState( true );
+	const
 
-	React.useEffect( () => {
-		fetch( 'https://63232eaf362b0d4e7dde21f1.mockapi.io/items' )
-			.then( ( res ) => res.json() )
-			.then( ( arr ) => {
-				setPizzas( arr );
-				setIsLoading( false );
-			} );
-	}, [] );
+		React.useEffect( () => {
+			fetch( 'https://63232eaf362b0d4e7dde21f1.mockapi.io/items' )
+				.then( ( res ) => res.json() )
+				.then( ( arr ) => {
+					setPizzas( arr );
+					setIsLoading( false );
+				} );
+
+			window.scrollTo( 0, 0 );
+		}, [] );
 
 	return (
 		<>
-			<div className="content__top">
-				<Categories />
-				<Sort />
-			</div>
-			<h2 className="content__title">All pizzas</h2>
-			<div className="content__items">
-				{ isLoading
-					? [ ...new Array( 6 ) ].map( ( _, i ) => <Sceleton key={ i } /> )
-					: pizzas.map( ( pizza, i ) => (
-						<PizzaBlock key={ i } { ...pizza } />
-					) )
-				}
+			<div className="container">
+				<div className="content__top">
+					<Categories value={ } />
+					<Sort />
+				</div>
+				<h2 className="content__title">All pizzas</h2>
+				<div className="content__items">
+					{ isLoading
+						? [ ...new Array( 6 ) ].map( ( _, i ) => <Sceleton key={ i } /> )
+						: pizzas.map( ( pizza, i ) => (
+							<PizzaBlock key={ i } { ...pizza } />
+						) )
+					}
+				</div>
 			</div>
 		</>
 	)
